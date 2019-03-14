7681e6a7e21abbfb60eeff8427f70b1dc1c8c68f
Hugo Collado <hugo.collado91@gmail.com>
2019-03-14 17:59:48 +0000

    added experiences controller

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,19 @@
+class ExperiencesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
