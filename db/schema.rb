501a43c9098806e9d0af7ff0fe0c784f1d7d27b6
Hugo Collado <hugo.collado91@gmail.com>
2019-03-14 17:41:25 +0000

    let's see

--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2019_03_11_221614) do
     t.string "languages"
     t.text "description"
     t.string "emergency_contact"
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
