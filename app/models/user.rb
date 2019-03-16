a485a827690415fa6447daf8f39151e6a5e76b6d
Hugo Collado <hugo.collado91@gmail.com>
2019-03-16 21:00:02 +0000

    added validations for user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
   validates :birthday_date, presence: true
 
   has_many :bookings
