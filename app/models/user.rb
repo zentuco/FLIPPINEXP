3452004b8aa3463cca2a073f82c9771488ac4ac9
Hugo Collado <hugo.collado91@gmail.com>
2019-03-16 21:23:06 +0000

    lets see

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-
   validates :birthday_date, presence: true
 
   has_many :bookings
