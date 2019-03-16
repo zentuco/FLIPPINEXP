7b837e27baef4ecc2dbeb1900a96ae6e624d0711
Hugo Collado <hugo.collado91@gmail.com>
2019-03-16 18:10:08 +0000

    added first name, last name and birthday as required

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :birthday_date, presence: true
+
   has_many :bookings
 
 end
